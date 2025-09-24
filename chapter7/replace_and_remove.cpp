44501f5a22e420008cd57097bdb2ccfe7fcb38a8
Success <successdaniel49@gmail.com>
2025-09-24 15:56:29 +0200

    Update replace and remove with the two variants.

--- chapter7/replace_and_remove.cpp
+++ chapter7/replace_and_remove.cpp
@@ -11,7 +11,11 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
+/*
+ * Time complexity: O(n)
+ */
 int replaceAndRemove(int size, char s[])
 {
   // Forward iteration: remove "b"s and count the number of "a"s
@@ -45,13 +49,53 @@ int replaceAndRemove(int size, char s[])
   return (final_size);
 }
 
+/*
+ * The second variant, with telex encoding is solved similar to 
+ * the solution above, first get the length of A and for
+ * each punctuation add in the length of the A for the first pass.
+ * For the second pass we start from the back of A maintaining a 
+ * write and a read pointer. The read pointer starts from the
+ * last char in A while write index starts from total length of 
+ * A with the punctuation - 1. so while read pointer is greater 
+ * than or equal to zero, we copy read into write, and the 
+ * punctuation backwards.
+ * Third variant:
+ * Merge two sorted arrays, assuming A has enough size to hold both arrays.
+ */
+
+void  mergeTwoSortedArrays(std::vector<int>& A, int m,
+    const std::vector<int>& B, int n)
+{
+  int i{m - 1}, j{n - 1}, k{m + n + 1};
+
+  while (j >= 0)
+  {
+    if (i >= 0 && A[i] > B[j])
+    {
+      A[k] = A[i];
+      --i;
+    }
+    else 
+    {
+      A[k] = B[j];
+      --j;
+    }
+    --k;
+  }
+}
+
 int main()
 {
-  char s[]{ 'a', 'c', 'b', 'a', 'b', 'a', 'b' };
-  int size { replaceAndRemove(7, s) };
+  //char s[]{ 'a', 'c', 'b', 'a', 'b', 'a', 'b' };
+  //int size { replaceAndRemove(7, s) };
   
-  for (int i{}; i < size; ++i)
-    std::cout << s[i] << " ";
+  std::vector<int> A;
+  A.reserve(6);
+  A[0] = 1, A[1] = 3, A[2] = 5;
+  std::vector<int> B{2, 4, 6};
+  mergeTwoSortedArrays(A, 3, B, 3);
+  for (size_t i{}; i < A.capacity(); ++i)
+    std::cout << A[i] << " ";
   std::cout << std::endl;
   return (0);
 }
